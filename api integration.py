1f7c4424ffc0a980af9026d2b125fb4de5505f71
Yashikatomar24 <yashika.tomar.ug23@nsut.ac.in>
2025-02-11 23:32:10 +0530

    api integration.py

--- api integration.py
+++ api integration.py
@@ -1,4 +1,4 @@
-import requests                                                                                                                                                                                              import requests
+import requests                                                                                                                                                                                      
 import datetime
 
 API_KEY = "YOUR_API_KEY"
