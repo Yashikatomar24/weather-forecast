69a265f1a86e2381d75ea530bf0e5a96709c6028
Yashikatomar24 <yashika.tomar.ug23@nsut.ac.in>
2025-02-16 01:14:53 +0530

    Update crop recommendation.py

--- crop recommendation.py
+++ crop recommendation.py
@@ -1,7 +1,7 @@
 
 import pandas as pd
 
-data = pd.read_csv("crop_production.csv")
+data = pd.read_csv("crop_yield_100.csv")
 
 print(data.head())
 import pandas as pd
@@ -9,9 +9,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 
-features = ['State_name' , 'Season']
+features = ['State', 'Season', 'Area', 'Soil_Type', 'Annual_Rainfall']
 X = data[features]
-y = data['crop']
+y = data['Crop']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
